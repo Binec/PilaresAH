cbef6ecd464844ce7f8466ac5d2ea141a06ee9eb
Binec <63314207+Binec@users.noreply.github.com>
2024-07-02 23:09:19 +0000

    galeria

--- js/David.js
+++ js/David.js
@@ -11,3 +11,20 @@
         })
     })
     }
+    */
+    function popup() {
+        var x = document.getElementById("popup");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+    function popregresar() {
+        var x = document.getElementById("popup");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+    }
